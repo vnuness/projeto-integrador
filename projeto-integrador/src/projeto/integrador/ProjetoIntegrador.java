c7f15ea0e8c8448f613728178e9dde4360f241fd
ZEUS <ZEUS@NBSP01.LINDOR>
2018-03-17 12:35:05 -0300

    Teste Paulo

--- projeto-integrador/src/projeto/integrador/ProjetoIntegrador.java
+++ projeto-integrador/src/projeto/integrador/ProjetoIntegrador.java
@@ -1,23 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projeto.integrador;
 
-/**
- *
- * @author vsilva
- */
+import java.util.Scanner;
+
+
 public class ProjetoIntegrador
 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args)
     {
+        Scanner input = new Scanner (System.in);
         System.out.println("Hello World!");
+        System.out.println("Teste Integração PC NB Paulo");
     }
     
 }
