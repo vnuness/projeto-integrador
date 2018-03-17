b05147f9663926029a72774d149e6bb53f6d4928
MEETAWEB\vsilva <vsilva@meeta.com.br>
2018-03-16 22:01:50 -0300

    Hello World

--- projeto-integrador/src/projeto/integrador/ProjetoIntegrador.java
+++ projeto-integrador/src/projeto/integrador/ProjetoIntegrador.java
@@ -17,7 +17,7 @@ public class ProjetoIntegrador
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
