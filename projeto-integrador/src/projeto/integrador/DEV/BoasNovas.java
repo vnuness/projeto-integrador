81c3a5b77df704031a5e92f92c7175c81281a1c1
MEETAWEB\vsilva <vsilva@meeta.com.br>
2018-04-17 13:52:53 -0300

    Criação da tela de Boas-Vindas

--- projeto-integrador/src/projeto/integrador/DEV/BoasNovas.java
+++ projeto-integrador/src/projeto/integrador/DEV/BoasNovas.java
@@ -1,16 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projeto.integrador.DEV;
 
-/**
- *
- * @author victor.snsilva
- */
-public class BoasNovas {
-    public static void main(String[] args) {
-        
+public class BoasNovas
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("                 UNIVERSIDADE DE TECNOLOGIA DO BRASIL            \n"
+                + "                 Olá !! Seja muito bem vindo à UBT!                \n"
+                + "                             Sobre nós: \n"
+                + "A UTB é o centro de excelência em tecnologia mais respeitado do país.\n"
+                + "Uma história que vem sendo construída dia após dia, estimulando mentes a vivenciarem a inovação\n"
+                + "e o empreendedorismo. Porque a UTB acredita no poder transformador da tecnologia.\n"
+                + "Um poder que, aliado à criatividade, inventa, muda, inquieta. Faz as pessoas buscarem soluções\n"
+                + "que nunca foram pensadas. Tudo isso para construir um mundo melhor.");
     }
 }
