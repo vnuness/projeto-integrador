dfacabc686a5362d9d9b573c600b885f5eb6dbdc
MEETAWEB\vsilva <vsilva@meeta.com.br>
2018-04-19 13:49:40 -0300

    Ajustes Teste Vocacional

--- projeto-integrador/src/projeto/integrador/DEV/TesteVocacional.java
+++ projeto-integrador/src/projeto/integrador/DEV/TesteVocacional.java
@@ -4,26 +4,29 @@ import java.util.Scanner;
 
 public class TesteVocacional
 {
+
     public static void main(String[] args)
     {
         int opcao;
         Scanner input = new Scanner(System.in);
-        do
-        {
+
         System.out.println("Olá Fulano, recebemos seus dados de inscrição com sucesso !\n"
                 + "Agora, \n"
-                + "Você irá participar do nosso teste vocacional para te ajudar ainda mais a traçar seu caminho. \n"
-                + "\n"
-                + "Digite [1] para iniciar o teste\n"
-                + "Digite [2] para sair");
-                opcao = input.nextInt();
-                if(opcao == 2)
-                {
-                    System.exit(0);
-                } else if (opcao != 1 && opcao != 2)
-                {
-                    System.out.println("Opção inválida!");
-                }
-        } while(opcao != 1);
+                + "Você irá participar do nosso teste vocacional para te ajudar ainda mais a traçar seu caminho. \n");
+        do
+        {
+            System.out.println("Digite [1] para iniciar o teste\n"
+                    + "Digite [2] para sair"
+            );
+            opcao = input.nextInt();
+            if (opcao == 2)
+            {
+                System.exit(0);
+            } else if (opcao != 1 && opcao != 2)
+            {
+                System.out.println("Opção inválida!");
+            }
+        } while (opcao != 1);
+
     }
 }
