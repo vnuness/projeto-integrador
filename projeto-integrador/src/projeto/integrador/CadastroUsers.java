ff70089da15481203b2f9f5eb297b1a7981b2c14
Eduardo Chagas <eduardo_cns@hotmail.com>
2018-04-21 10:47:27 -0300

    Inclui algumas perguntas no cadastro.

--- projeto-integrador/src/projeto/integrador/CadastroUsers.java
+++ projeto-integrador/src/projeto/integrador/CadastroUsers.java
@@ -9,7 +9,7 @@ public class CadastroUsers {
         
         System.out.println("Para realizar seu cadastro precisamos que responda as seguintes perguntas: ");
         
-        System.out.println("Informe seu nome completo: :");
+        System.out.println("Informe seu nome e sobrenome :");
         String nome = entrada.next();
         
         System.out.println("Informe o nome que gostaria de ser chamado no decorrer do curso:");
@@ -20,6 +20,9 @@ public class CadastroUsers {
         
         System.out.println("Informe seu e-mail: ");
         String email = entrada.next();
+        
+        System.out.println("Está trabalhando? Se sim informar qual área: ");
+        String empregado = entrada.next();
         
         System.out.println("Possui alguma graduação? se sim informar o nome do curso: ");
         String outraGraduacao = entrada.next();
