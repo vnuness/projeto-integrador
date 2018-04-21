f0a339407c1bb1b7419f74d67df7d90cb92be82c
Eduardo Chagas <eduardo_cns@hotmail.com>
2018-04-21 12:18:09 -0300

    Inclui algumas perguntas no cadastro.

--- projeto-integrador/src/projeto/integrador/DEV/CadastroUsers.java
+++ projeto-integrador/src/projeto/integrador/DEV/CadastroUsers.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projeto.integrador.DEV;
 
-/**
- *
- * @author victor.snsilva
- */
+import java.util.Scanner;
+
 public class CadastroUsers {
-    
+
+    public void cadastro() {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Para realizar seu cadastro precisamos que responda as seguintes perguntas: ");
+
+        System.out.println("Informe seu nome e sobrenome :");
+        String nome = entrada.next();
+
+        System.out.println("Informe o nome que gostaria de ser chamado no decorrer do curso:");
+        String apelido = entrada.next();
+
+        System.out.println("Informe sua idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.println("Informe seu e-mail: ");
+        String email = entrada.next();
+
+        System.out.println("Está trabalhando? Se sim informar qual área: ");
+        String empregado = entrada.next();
+
+        System.out.println("Possui alguma graduação? se sim informar o nome do curso: ");
+        String outraGraduacao = entrada.next();
+
+    }
 }
