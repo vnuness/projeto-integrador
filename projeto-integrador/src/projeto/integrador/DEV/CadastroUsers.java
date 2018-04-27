69e6a6485df4be7dd070008d146ef8fc41d9db00
MEETAWEB\vsilva <vsilva@meeta.com.br>
2018-04-27 20:50:19 -0300

    Revert "Ajustes cadastro de Usuários"
    This reverts commit 7176622f61cc40ea2ac27885c8f1a9cc5596077f.

--- projeto-integrador/src/projeto/integrador/DEV/CadastroUsers.java
+++ projeto-integrador/src/projeto/integrador/DEV/CadastroUsers.java
@@ -4,10 +4,8 @@ import java.util.Scanner;
 
 public class CadastroUsers {
 
-    public static void main (String[] args) {
+    public void cadastro() {
         Scanner entrada = new Scanner(System.in);
-        Scanner entrada2 = new Scanner(System.in);
-        int idade;
 
         System.out.println("Para realizar seu cadastro precisamos que responda as seguintes perguntas: ");
 
@@ -15,19 +13,19 @@ public class CadastroUsers {
         String nome = entrada.next();
 
         System.out.println("Informe o nome que gostaria de ser chamado no decorrer do curso:");
-        String apelido = entrada2.nextLine();
+        String apelido = entrada.next();
 
         System.out.println("Informe sua idade: ");
-        idade = entrada2.nextInt();
+        int idade = entrada.nextInt();
 
         System.out.println("Informe seu e-mail: ");
-        String email = entrada2.next();
+        String email = entrada.next();
 
         System.out.println("Está trabalhando? Se sim informar qual área: ");
-        String empregado = entrada2.next();
+        String empregado = entrada.next();
 
         System.out.println("Possui alguma graduação? se sim informar o nome do curso: ");
-        String outraGraduacao = entrada2.next();
+        String outraGraduacao = entrada.next();
 
     }
 }
